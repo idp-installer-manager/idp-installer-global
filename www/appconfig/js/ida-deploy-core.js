83bc0136077bd0b505e7c57afbd2672351106142
Chris Phillips <chris.phillips@canarie.ca>
2014-01-20 12:08:58 -0500

    added: tooltips, updated reference material
    Enhanced www/appconfig/CAF/index.html to add jQUery tooltips for
    location sensitive help info.
    First Rev of documentation added.

--- www/appconfig/js/ida-deploy-core.js
+++ www/appconfig/js/ida-deploy-core.js
@@ -404,6 +404,15 @@ var cNeutral = "#FFFFFF"
             console.log('Update():begin mapping');
         }
 
+
+// enable tooltip support for all tags
+        if (loggingEnabled) {
+            console.log('Update():enabling tooltips');
+        }
+
+$(document).ready(function() {            $("[rel='tooltip']").tooltip();     });
+
+
         //////////////////////////// my_eduroamDomain
 
         if (loggingEnabled) {
